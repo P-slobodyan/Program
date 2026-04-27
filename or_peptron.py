bcf77283b4c21d4ef9703845b294686ffeb51393
P-slobodyan <p.slobodyan2001@yandex.ru>
2026-04-27 23:56:23 +0300

    Изменил шаг и ошобку, столо лучше, но 0-й выход попрежнему 0.5

--- or_peptron.py
+++ or_peptron.py
@@ -2,9 +2,9 @@ import torch as pt
 import torch.nn as nn 
 import torch.autograd as agrad
 
-epochs = 500000         # Количество эпох
-h = 0.01                # Шаг обучения (скорость)
-error_epochs = 0.001    # Допустимая ошибка
+epochs = 500000        # Количество эпох
+h = 0.1                # Шаг обучения (скорость)
+error_epochs = 0.05    # Допустимая ошибка
 
 w = pt.tensor([[0.75], [0.53]], requires_grad=True)                 # Веса
 x = pt.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=pt.float32)   # Входные данные
@@ -28,3 +28,4 @@ for index in range(epochs):
         w.grad.zero_()  # Обнуляем градиенты после обновления весов
 
 print(f"Выходы после обучения: {sigm_fun.data}")
+print(f"Невязка после обучения: {incons.item()}")
